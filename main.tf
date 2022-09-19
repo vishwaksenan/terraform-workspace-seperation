c984e581b2db173148a69e805f14c52495c9fb3e
vishwaksenan <vishwaksenang@gmail.com>
2022-09-19 08:28:55 +0100

    terraform lambda

--- main.tf
+++ main.tf
@@ -26,3 +26,37 @@ resource "aws_instance" "temp_aws_instance" {
     Name = "${terraform.workspace == "temp-app-prod" ? "prodEc2" : "devEc2"}"
   }
 }
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "archive_file" "python_lambda_hello_world" {
+  type = "zip"
+  source_dir = "${path.module}/python-lambda"
+  output_path = "${path.module}/python-lambda/python-lambda.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda" {
+  filename = "${path.module}/python-lambda/python-lambda.zip"
+  function_name = "first_lambda"
+  role = aws_iam_role.iam_for_lambda.arn
+  handler = "hello-world.lambda_handler"
+  runtime = "python3.8"
+}
